2e926c8c1e6c1f8672d883c5765ef07bf2bf3dc3
Александр Мигаль <magalalecs2003@gmail.com>
2023-12-25 23:25:54 +0200

    serv

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Маршрут головної сторінки
 app.get("/", (req, res) => {
   res.send("Це головна сторінка!");
+});
+
+// Маршрут для /about
+app.get("/about", (req, res) => {
+  res.send('Це сторінка "Про нас".');
+});
+
+// Маршрут для /contact
+app.get("/contact", (req, res) => {
+  res.send('Це сторінка "Контакти".');
+});
+
+// Слухати запити на вибраному порту
+app.listen(port, () => {
+  console.log(`Сервер запущено на порті ${port}`);
 });

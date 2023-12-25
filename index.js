a3a28b0603277e261221b11fbef7925b9c7a623d
Александр Мигаль <magalalecs2003@gmail.com>
2023-12-25 23:02:24 +0200

    fix

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const i = 2 * 2;
-console.log(i);
+const handler = async (event, context) => {
+  const i = 2 * 2;
+  console.log(i);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ result: i }),
+  };
+};
+
+module.exports = { handler };
